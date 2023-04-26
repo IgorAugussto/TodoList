203c15104ff12c90433ffcdb3b1c12eabf102885
Igor Augusto <igor-augustofc@hotmail.com>
2023-04-26 14:21:26 -0300

    Added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+//Liberando acesso do backen para client
+const cors = require('cors');
 //Importando o Router para o App
 const router = require('./router');
+
 
 const app = express();
 //Desse modo a API vai conseguir trabalhar com dados JSON
 app.use(express.json());
 //Dizer para o App usar sempre o Router para fazer as requisições
 app.use(router);
+app.use(cors());
 
 app.get('/', (req, res) => res.status(200).send('Hello, world'));
 
